5e8fc97d5ca4ca10d9c4139b888dd9c4266b107b
Dream5445 <artem.oleynik.4321@gmail.com>
2022-04-11 11:32:16 +0300

    Create HelperHelp.cs

---
+++ HelperHelp.cs
@@ -0,0 +1,27 @@
+﻿using ImTools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task4.Helper
+{
+    class HelperHelp
+    {
+        public void Random (ref string randomWord)
+        {
+            bool digitOrLetter = false;
+            Random random = new Random();
+            for (int i = 0; i < 14; i++){
+                if (digitOrLetter)
+                {
+                    randomWord += (char)random.Next(65, 90);
+                }
+                else
+                {
+                    randomWord += (char)random.Next(48, 57);
+                }
+                digitOrLetter = !digitOrLetter;
+            }
+        }
+    }
+}

9db470586e3132902a9a455f04f9662f56ce6fbd
Dream5445 <artem.oleynik.4321@gmail.com>
2022-04-11 11:25:01 +0300

    Create RegistrationStep.cs

---
+++ RegistrationStep.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Task_1.Implement;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Task_1.Features
+{
+    [Binding]
+    public class Registration
+    {
+        public IWebDriver driver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/25-virtual-gift-card");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+
+        [Given(@"Clik on the button Registration")]
+        public void GivenClikOnTheButtonRegistration()
+        {
+            RegistrationImplement reg = new RegistrationImplement(driver);
+            reg.clikOnBtn();
+        }
+        
+        [Then(@"Fill in the data fields")]
+        public void ThenFillInTheDataFields(Table table)
+        {
+            RegistrationImplement reg = new RegistrationImplement(driver);
+            reg.AutoRegistration("Artem", "Oleynik", "artem.oleynik.2426@gmail.com", "qwerty", "qwerty");
+        }
+        
+        [Then(@"Click on the Registers")]
+        public void ThenClickOnTheRegisters()
+        {
+            RegistrationImplement reg = new RegistrationImplement(driver);
+            reg.regisrBtn();
+        }
+    }
+}

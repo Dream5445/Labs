2676d478d910ccc282abd1d3d0ee9e8fa5d4ef13
Dream5445 <artem.oleynik.4321@gmail.com>
2022-04-11 11:32:11 +0300

    Create SendMassegImplement.cs

---
+++ SendMassegImplement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using Task4.POM;
+
+namespace Task4.Implement
+{
+    class SendMassegImplement
+    {
+        IWebDriver _driver;
+
+        public SendMassegImplement(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void ClikOnBtn()
+        {
+            SendMessengPOM send = new SendMessengPOM(_driver);
+            send.Contact();
+        }
+
+        public void Sumbit()
+        {
+            SendMessengPOM send = new SendMessengPOM(_driver);
+            send.Sumbit();
+        }
+
+    }
+}

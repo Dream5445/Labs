72f362a700cf0baa0471a13b301b41d33e00f538
Dream5445 <artem.oleynik.4321@gmail.com>
2022-04-11 11:35:23 +0300

    Create AddKreditCardImplement.cs

---
+++ AddKreditCardImplement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using Task5_6.POM;
+
+namespace Task5_6.Implement
+{
+    class AddKreditCardImplement
+{
+        IWebDriver _driver;
+
+        public AddKreditCardImplement(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void GiftCard()
+        {
+            AddKreditCardPOM add = new AddKreditCardPOM(_driver);
+            add.clikonBtn();
+
+
+        }
+
+        public void ClikOnBtn()
+        {
+            AutorizationPOM autho = new AutorizationPOM(_driver);
+            autho.clikonBtn();
+        }
+
+        public void ClikOnBtnSignIn()
+        {
+            AutorizationPOM autho = new AutorizationPOM(_driver);
+            autho.clikonBtnLoginn();
+        }
+    }
+}

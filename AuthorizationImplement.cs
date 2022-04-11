3d6770f8cdfa2b02019ce448a34c07b2ff16d1d5
Dream5445 <artem.oleynik.4321@gmail.com>
2022-04-11 11:25:18 +0300

    Create AuthorizationImplement.cs

---
+++ AuthorizationImplement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using Task_1.POM;
+
+namespace Task_1.Implement
+{
+    class AuthorizationImplement
+    {
+        IWebDriver _driver;
+
+        public AuthorizationImplement(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Autho(string email, string password)
+        {
+            AuthorizationPOM autho = new AuthorizationPOM(_driver);
+            autho.email(email);
+            autho.password(password);
+        }
+
+        public void ClikOnBtn()
+        {
+            AuthorizationPOM autho = new AuthorizationPOM(_driver);
+            autho.clikonBtn();
+        }
+
+        public void ClikOnBtnSignIn()
+        {
+            AuthorizationPOM autho = new AuthorizationPOM(_driver);
+            autho.clikonBtnLoginn();
+        }
+    }
+}

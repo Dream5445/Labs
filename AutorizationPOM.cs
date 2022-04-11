9be09e2211acfb9a5ec3f7518e8a4f7c29df707d
Dream5445 <artem.oleynik.4321@gmail.com>
2022-04-11 11:22:44 +0300

    Create AutorizationPOM.cs

---
+++ AutorizationPOM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Task2.POM
+{
+    class AutorizationPOM
+    {
+
+        IWebDriver _driver;
+        public AutorizationPOM(IWebDriver driver)
+        {
+            _driver = driver;
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+
+        private readonly static By _clikonBtn = By.XPath("//a[@class='ico-login']");
+        private readonly static By _email = By.Id("Email");
+        private readonly static By _password = By.Id("Password");
+        private readonly static By _clikonBtnLogin = By.XPath("//input[@class='button-1 login-button']");
+
+        public void clikonBtn()
+        {
+            _driver.FindElement(_clikonBtn).Click();
+        }
+
+        public AutorizationPOM email(string text)
+        {
+            _driver.FindElement(_email).SendKeys(text);
+            return this;
+        }
+
+        public AutorizationPOM password(string text)
+        {
+            _driver.FindElement(_password).SendKeys(text);
+            return this;
+        }
+
+        public void clikonBtnLoginn()
+        {
+            _driver.FindElement(_clikonBtnLogin).Click();
+        }
+    }
+}

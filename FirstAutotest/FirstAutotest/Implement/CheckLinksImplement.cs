0ed9b93b571ad12041d471cf689ac622d15fa365
Dream5445 <artem.oleynik.4321@gmail.com>
2022-05-14 19:45:32 +0300

    Create CheckLinksImplement.cs

---
+++ FirstAutotest/FirstAutotest/Implement/CheckLinksImplement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using FirstAutotest.POM;
+
+namespace FirstAutotest.Implement
+{
+    class CheckLinksImplement
+    {
+        IWebDriver _driver;
+
+        public CheckLinksImplement(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void ClikOnBtn()
+        {
+            CheckLinksPOM wev = new CheckLinksPOM(_driver);
+            wev.Clickfirst();
+        }
+        public void ClikOnBtnsecond()
+        {
+            CheckLinksPOM wev = new CheckLinksPOM(_driver);
+            wev.Clicksecond();
+        }
+        public void ClikOnBtnthird()
+        {
+            CheckLinksPOM wev = new CheckLinksPOM(_driver);
+            wev.Clickthird();
+        }
+    }
+}

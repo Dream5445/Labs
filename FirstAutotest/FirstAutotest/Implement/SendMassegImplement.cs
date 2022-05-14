3d54893c9328eda00a0d4d34b5376ad61428c460
Dream5445 <artem.oleynik.4321@gmail.com>
2022-05-14 19:46:35 +0300

    Create SendMassegImplement.cs

---
+++ FirstAutotest/FirstAutotest/Implement/SendMassegImplement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using FirstAutotest.POM;
+
+namespace FirstAutotest.Implement
+{
+    class SendMassegImplement
+    {
+        IWebDriver _driver;
+
+        public SendMassegImplement(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void ClikOnBtn()
+        {
+            SendMessengPOM send = new SendMessengPOM(_driver);
+            send.Contact();
+        }
+
+        public void Sumbit()
+        {
+            SendMessengPOM send = new SendMessengPOM(_driver);
+            send.Sumbit();
+        }
+
+    }
+}

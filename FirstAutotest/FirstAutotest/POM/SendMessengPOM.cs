084030b35e6696731dae93c48ef70d8b9c8c4327
Dream5445 <artem.oleynik.4321@gmail.com>
2022-05-14 19:46:21 +0300

    Create SendMessengPOM.cs

---
+++ FirstAutotest/FirstAutotest/POM/SendMessengPOM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using FirstAutotest.Helper;
+
+namespace FirstAutotest.POM
+{
+    class SendMessengPOM
+    {
+        IWebDriver _driver;
+        public SendMessengPOM(IWebDriver driver)
+        {
+            _driver = driver;
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+
+        private readonly static By _ContactUs = By.XPath("//a[@href='/contactus']");
+        private readonly static By _Enquiry = By.Id("Enquiry");
+        private readonly static By _Submit = By.XPath("//input[@name='send-email']");
+
+        public void Contact()
+        {
+            _driver.FindElement(_ContactUs).Click();
+            Thread.Sleep(2000);
+        }
+
+        public void Enq() {
+            HelperHelp hel = new HelperHelp();
+            string randomWord = " ";
+            hel.Random(ref randomWord);
+            _driver.FindElement(_Enquiry).SendKeys(randomWord);
+            Thread.Sleep(2000);
+        }
+
+        public void Sumbit()
+        {
+            _driver.FindElement(_Submit).Click();
+            Thread.Sleep(2000);
+        }
+    }
+}

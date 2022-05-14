9a965751fa691c598092cd30879d00c48d36e476
Dream5445 <artem.oleynik.4321@gmail.com>
2022-05-14 19:45:30 +0300

    Create AutorigestrationImplement.cs

---
+++ FirstAutotest/FirstAutotest/Implement/AutorigestrationImplement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using FirstAutotest.POM;
+
+namespace FirstAutotest.Implement
+{
+    class AutorigestrationImplement
+    {
+        IWebDriver _driver;
+
+        public AutorigestrationImplement(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Autho(string email, string password)
+        {
+            AutorigestrationPOM autho = new AutorigestrationPOM(_driver);
+            autho.email(email);
+            autho.password(password);
+        }
+
+        public void ClikOnBtn()
+        {
+            AutorigestrationPOM autho = new AutorigestrationPOM(_driver);
+            autho.clikonBtn();
+        }
+
+        public void ClikOnBtnSignIn()
+        {
+            AutorigestrationPOM autho = new AutorigestrationPOM(_driver);
+            autho.clikonBtnLoginn();
+        }
+    }
+}
